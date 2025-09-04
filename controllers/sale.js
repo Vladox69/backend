caa2845e3e6efdf39b2e6be39ebfb3828a4c3b1b
Vladimir Aguacunchi <marwladiagua2001@hotmail.com>
2025-09-03 21:31:43 -0500

    feat: update generateXmlInvoice to use environment variables for certificate URL and password

--- controllers/sale.js
+++ controllers/sale.js
@@ -391,12 +391,7 @@ const generateXmlInvoice = async (req, res = response) => {
     }
 
     const xmlString = doc.end({ prettyPrint: false }).toString().trim();
-    const certUrl =
-      "https://calidad.atiendo.ec/eojgprlg/Certificados/11578175_identity_1803480399.p12";
-    /*const publicId = "cert/11578175_identity_1803480399.p12";
-    const signedUrl = cloudinary.utils.private_download_url(publicId, "raw", {
-      expires_at: Math.floor(Date.now() / 1000) + 60 * 5, 
-    });*/
+    const certUrl =process.env.CERT_URL;
     const p12Password = process.env.PASS_CERT;
     const xmlFirmado = await signXml(certUrl, p12Password, xmlString);
     
